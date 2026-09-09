372dbce4fbc3145b479d72fdd1d7c73670555eca
Alexander Makletsov <tegmore@icloud.com>
2026-09-09 14:30:22 +0300

    feat: add i18n support for English and Russian

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en', 'ru'],
+	},
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
